26f12e8bf626e08f412f00ae52cedfa584ff8a5d
lucasrafa2008 <lucasrafa2008@gmail.com>
2022-07-18 23:15:22 -0300

    Adding migrations and models

--- database/migrations/2022_07_19_013243_server_connections.php
+++ database/migrations/2022_07_19_013243_server_connections.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class ServerConnections extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+        Schema::create('ServerConnections', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('organization_name');
+            $table->string('ipadress');
             $table->string('created_by');
-            $table->rememberToken();
             $table->timestamps();
+
         });
     }
 
@@ -32,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        //
     }
 }
